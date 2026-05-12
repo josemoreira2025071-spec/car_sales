d14c5c9026e3f210d79c1e13c8bd09ec17e4852a
josemoreira2025071-spec <jose.moreira.2025071@my.istec.pt>
2026-05-12 11:40:09 +0000

    Configuração do Docker Compose

--- car_sales/urls.py
+++ car_sales/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
+# Define urlpatterns PRIMEIRO
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cars.urls')),
 ]
+
+# Depois adiciona as rotas de media (só em desenvolvimento)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
