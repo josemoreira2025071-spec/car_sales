3e4a8bda724da2a63b42b2797e2e7fe353c16670
josemoreira2025071-spec <jose.moreira.2025071@my.istec.pt>
2026-05-12 11:33:25 +0000

    Modelos de base de dados (Car) e migração inicial

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='cars/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
