9df64e85fc79ab0d34237a44a63bf21a7fb96d05
josemoreira2025071-spec <jose.moreira.2025071@my.istec.pt>
2026-05-12 11:47:22 +0000

    Desenvolvimento da página de detalhe do carro

--- cars/urls.py
+++ cars/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('cars/', views.car_list, name='car_list'),
+    path('car/<int:pk>/', views.car_detail, name='car_detail'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Car
 
+# Página inicial
+def home(request):
+    return render(request, 'cars/home.html')
+
+
+# Página de listagem de todos os carros
 def car_list(request):
     cars = Car.objects.all().order_by('-created_at')
     return render(request, 'cars/car_list.html', {'cars': cars})
+
+
+# Página de detalhe de um carro específico
+def car_detail(request, pk):
+    car = get_object_or_404(Car, pk=pk)
+    return render(request, 'cars/car_detail.html', {'car': car})
